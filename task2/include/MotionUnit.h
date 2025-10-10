9b9cba458a5eae728acd8e8e05fd24aa5ec55637
Muhammad Fahmi Ilmi <128874558+fahmiilmi14@users.noreply.github.com>
2025-10-10 09:37:37 +0700

    Add files via upload

---
+++ task2/include/MotionUnit.h
@@ -0,0 +1,11 @@
+#ifndef MOTION_UNIT_H
+#define MOTION_UNIT_H
+
+#include <string>
+
+class MotionUnit{
+    public:
+        void loadDanSimpan(const std::string& path,int id, int ulang);
+};
+
+#endif
