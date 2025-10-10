44c74458f96ba6745ad8b009d302462a0ff43f35
Muhammad Fahmi Ilmi <128874558+fahmiilmi14@users.noreply.github.com>
2025-10-10 09:36:51 +0700

    Create MotionMovie.h

---
+++ task2/include/MotionMovie.h
@@ -0,0 +1,12 @@
+#ifndef MOTION_MOVIE_H
+#define MOTION_MOVIE_H
+
+#include <string>
+#include "MotionUnit.h"
+
+class MotionMovie{
+    public :
+        void load(const std::string& path);
+};
+
+#endif
